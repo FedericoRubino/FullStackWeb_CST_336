96089712e3decdeeeb33e23aa88cf83a8b2b33e5
FedericoRubino <frubino@csumb.edu>
2020-03-11 19:13:28 -0700

    fixes

--- index.js
+++ index.js
@@ -4,15 +4,17 @@
 	3. Tell NodeJS to use the ejs as page 
 
 */
-console.log("running");
+// console.log("running");
 
 var express = require("express");
 var bodyParser = require("body-parser")
 var app = express();
 
-app.use(express.static("css"));
-app.use(bodyParser.urlencoded({extended:true}));
 app.set('view engine', 'ejs');
+
+app.use(express.static("css"));
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res){
 	res.send("home.ejs");
@@ -28,16 +30,18 @@ app.get("/friends", function(req, res){
 
 // the form method is post so we need to call app.post
 app.post("/addfriends", function(req, res){
-	console.log(req.body);
+	// console.log(req.body);
+	var newFriend = req.body.newFriend;
+	friendlist.push(newFriend);
+	res.redirect("/friends");
 });
 
 
-app.get("*", function(req, res){
+app.get("/*", function(req, res){
 	res.send("error.ejs");
 });
 
-
-app.listen(process.env.PORT || 3000, function(){
+//3000 is for localhost and 8080 is for heroku
+app.listen(process.env.PORT || 3000 || 8080, function(){
 	console.log("Server is running...");
-
 });
